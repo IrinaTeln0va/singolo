e93706933a1edc5dce99a87376f1a35395c88f7c
IrinaTeln0va <ira8365@mail.ru>
2020-03-15 21:40:02 +0300

    fix: wrap into onload function

--- script.js
+++ script.js
@@ -1,140 +1,142 @@
-// navigation
+window.onload = function () {
+        // navigation
 
-const navList = document.querySelector('.nav-list');
+    const navList = document.querySelector('.nav-list');
 
-navList.addEventListener('click', (evt) => {
-    navList.querySelectorAll('.service-link').forEach((link) => link.classList.remove('active'));
-    evt.target.classList.add('active');
-});
+    navList.addEventListener('click', (evt) => {
+        navList.querySelectorAll('.service-link').forEach((link) => link.classList.remove('active'));
+        evt.target.classList.add('active');
+    });
 
-// slider
+    // slider
 
-const slidesContainer = document.querySelector('.slides-list');
-const rightArrow = document.querySelector('.arrow-right');
-const leftArrow = document.querySelector('.arrow-left');
-const parentContainer = document.querySelector('.slider');
+    const slidesContainer = document.querySelector('.slides-list');
+    const rightArrow = document.querySelector('.arrow-right');
+    const leftArrow = document.querySelector('.arrow-left');
+    const parentContainer = document.querySelector('.slider');
 
-function nextSlide() {
-    slidesContainer.append(slidesContainer.firstElementChild);
-    slidesContainer.classList.remove('next-slide');
-    isAnimating = false;
-}
-
-function prevSlide() {
-    slidesContainer.prepend(slidesContainer.lastElementChild);
-    slidesContainer.classList.remove('prev-slide');
-    isAnimating = false
-}
-
-let isAnimating = false;
-
-rightArrow.addEventListener('click', () => {
-    if (isAnimating) {
-        return;
+    function nextSlide() {
+        slidesContainer.append(slidesContainer.firstElementChild);
+        slidesContainer.classList.remove('next-slide');
+        isAnimating = false;
     }
-    isAnimating = true;
-    slidesContainer.classList.add('next-slide');
-    parentContainer.classList.toggle('slider--bg-blue');
-    slidesContainer.addEventListener('transitionend', nextSlide, { once: true });
-});
 
-leftArrow.addEventListener('click', () => {
-    if (isAnimating) {
-        return;
+    function prevSlide() {
+        slidesContainer.prepend(slidesContainer.lastElementChild);
+        slidesContainer.classList.remove('prev-slide');
+        isAnimating = false
     }
-    isAnimating = true;
-    slidesContainer.classList.add('prev-slide');
-    parentContainer.classList.toggle('slider--bg-blue');
-    slidesContainer.addEventListener('transitionend', prevSlide, { once: true });
-});
 
-const mockup = document.querySelectorAll('.mockup-1-wrap');
+    let isAnimating = false;
 
-mockup.forEach(item => {
-    item.addEventListener('click', (evt) => {
-        if (evt.target.classList.contains('phone-shadow')) {
+    rightArrow.addEventListener('click', () => {
+        if (isAnimating) {
             return;
         }
-        item.classList.toggle('mockup-disabled');
-    })
-});
+        isAnimating = true;
+        slidesContainer.classList.add('next-slide');
+        parentContainer.classList.toggle('slider--bg-blue');
+        slidesContainer.addEventListener('transitionend', nextSlide, { once: true });
+    });
 
-// filters
+    leftArrow.addEventListener('click', () => {
+        if (isAnimating) {
+            return;
+        }
+        isAnimating = true;
+        slidesContainer.classList.add('prev-slide');
+        parentContainer.classList.toggle('slider--bg-blue');
+        slidesContainer.addEventListener('transitionend', prevSlide, { once: true });
+    });
 
-const filtersList = document.querySelectorAll('.filter-btn');
-const portfolioWrap = document.querySelector('.portfolio-projects');
-const SHIFT_IMAGE = 4;
+    const mockup = document.querySelectorAll('.mockup-1-wrap');
 
-const onFilterClick = (evt) => {
-    filtersList.forEach(item => item.classList.remove('active'));
-    evt.target.classList.add('active');
-    const portfolioList = [...portfolioWrap.querySelectorAll('.portfolio-item')];
-    const positionList = portfolioList.map((item, index) => index).sort((a, b) => Math.random() - 0.5);
+    mockup.forEach(item => {
+        item.addEventListener('click', (evt) => {
+            if (evt.target.classList.contains('phone-shadow')) {
+                return;
+            }
+            item.classList.toggle('mockup-disabled');
+        })
+    });
 
-    portfolioList.forEach((elem) => elem.remove());
-    for (let i = 0; i < portfolioList.length; i++) {
-        portfolioList[i].style.order = positionList[i] + 1;
-        portfolioList[i].classList.add('animated');
+    // filters
+
+    const filtersList = document.querySelectorAll('.filter-btn');
+    const portfolioWrap = document.querySelector('.portfolio-projects');
+    const SHIFT_IMAGE = 4;
+
+    const onFilterClick = (evt) => {
+        filtersList.forEach(item => item.classList.remove('active'));
+        evt.target.classList.add('active');
+        const portfolioList = [...portfolioWrap.querySelectorAll('.portfolio-item')];
+        const positionList = portfolioList.map((item, index) => index).sort((a, b) => Math.random() - 0.5);
+
+        portfolioList.forEach((elem) => elem.remove());
+        for (let i = 0; i < portfolioList.length; i++) {
+            portfolioList[i].style.order = positionList[i] + 1;
+            portfolioList[i].classList.add('animated');
+        }
+        portfolioWrap.append(...portfolioList);
+        setTimeout(() => {
+            portfolioList.forEach((item) => item.classList.remove('animated'));
+        }, 0);
     }
-    portfolioWrap.append(...portfolioList);
-    setTimeout(() => {
-        portfolioList.forEach((item) => item.classList.remove('animated'));
-    }, 0);
-}
 
-filtersList.forEach((filter) => {
-    filter.addEventListener('click', onFilterClick);
-});
-
-// portfolio
-
-portfolioWrap.addEventListener('click', (evt) => {
-    portfolioWrap.querySelectorAll('.portfolio-item').forEach((item) => {
-        item.classList.remove('active');
+    filtersList.forEach((filter) => {
+        filter.addEventListener('click', onFilterClick);
     });
-    evt.target.parentElement.classList.add('active');
-});
 
-//form
+    // portfolio
 
-const submitBtn = document.querySelector('.submit-btn');
-const form = document.querySelector('.request-form');
+    portfolioWrap.addEventListener('click', (evt) => {
+        portfolioWrap.querySelectorAll('.portfolio-item').forEach((item) => {
+            item.classList.remove('active');
+        });
+        evt.target.parentElement.classList.add('active');
+    });
 
-const generateMessageContent = (title = 'Без темы', descr ='Без описания') => {
-    return `<div class="messageInner"><h3 class="message-title">Письмо отправлено</h3>
-<div class="message-subject">${ title ? `Тема: <span class="insertedText">${title}` : `Без темы` }</div>
-<div class="message-descr">${ descr ? `Описание: </span><span class="insertedText">${descr}` : `Без описания` }</div>
-<button class="message-btn" type="button">OK</button></div>`
-}
+    //form
 
-const createMessageElem = (titleText, descrText) => {
-    messageElement = document.createElement('div');
-    messageElement.classList.add('overlay');
-    messageElement.innerHTML = `${generateMessageContent(titleText, descrText)}`;
-    return messageElement;
-}
+    const submitBtn = document.querySelector('.submit-btn');
+    const form = document.querySelector('.request-form');
 
-form.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    
-    const titleText = document.querySelector('.subject-input').value;
-    const descrText = document.querySelector('.desc-input').value;
-    const messageElem = createMessageElem(titleText, descrText);
+    const generateMessageContent = (title = 'Без темы', descr ='Без описания') => {
+        return `<div class="messageInner"><h3 class="message-title">Письмо отправлено</h3>
+    <div class="message-subject">${ title ? `Тема: <span class="insertedText">${title}` : `Без темы` }</div>
+    <div class="message-descr">${ descr ? `Описание: </span><span class="insertedText">${descr}` : `Без описания` }</div>
+    <button class="message-btn" type="button">OK</button></div>`
+    }
 
-    form.append(messageElem);
-    setTimeout(() => {
-        messageElem.classList.add('animated');
-    }, 0);
+    const createMessageElem = (titleText, descrText) => {
+        messageElement = document.createElement('div');
+        messageElement.classList.add('overlay');
+        messageElement.innerHTML = `${generateMessageContent(titleText, descrText)}`;
+        return messageElement;
+    }
 
-    addCloseHandler(messageElem);
-});
+    form.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        
+        const titleText = document.querySelector('.subject-input').value;
+        const descrText = document.querySelector('.desc-input').value;
+        const messageElem = createMessageElem(titleText, descrText);
 
-const addCloseHandler = (messageElem) => {
-    const messageBtn = document.querySelector('.message-btn');
-    
-    messageBtn.addEventListener('click', () => {
         form.reset();
-        messageElem.remove();
+        form.append(messageElem);
+        setTimeout(() => {
+            messageElem.classList.add('animated');
+        }, 0);
+
+        addCloseHandler(messageElem);
     });
+
+    const addCloseHandler = (messageElem) => {
+        const messageBtn = document.querySelector('.message-btn');
+        
+        messageBtn.addEventListener('click', () => {
+            messageElem.remove();
+        });
+    };
 };
 
